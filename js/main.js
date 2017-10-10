98ec49e29ed293571c5f04fcea10bf35359df0ee
solisqq <ebaza3@go2.pl>
2017-10-10 12:36:35 +0200

    Upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function (){
+
+	var header_size=0;
+	var toggle_focus=0;
+
+	/*Pages*/
+	main_start_pos = 0;
+	/*/Pages*/
+
+	$(window).scroll(function() {//OnScrollChange
+		var position = $(document).scrollTop(); //Position
+	});
+	$(".scroll_link").click(function (e){//Scroll Link
+		toggle_focus=1; 
+		e.preventDefault();
+		var destination = $(this).data('dest');
+		var scrollDestination = $(destination).offset().top-header_size;
+		this.focus();
+        $('html, body').animate({
+            scrollTop: scrollDestination
+        }, "slow", function(){ toggle_focus=0; });
+        console.log(destination);
+    });
+    $("#scroll_link").click(function (e){
+    	toggle_focus=1; 
+		e.preventDefault();
+		var destination = $(this).data('dest');
+		var scrollDestination = $(destination).offset().top-header_size;
+		this.focus();
+        $('html, body').animate({
+            scrollTop: scrollDestination
+        }, "slow", function(){ toggle_focus=0; });
+        console.log(destination);
+    });
+});
