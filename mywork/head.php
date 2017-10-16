995f15f524066a74b7109c53b71f33be068c1e92
solisqq <ebaza3@go2.pl>
2017-10-16 09:42:45 +0200

    V22

---
+++ mywork/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kamil Słotwiński</title>
+
+    <!-- Czcionka-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet"> 
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Mój CSS -->
+    <link href="css/main.css" rel="stylesheet">
+</head>
---
+++ mywork/menu.php
@@ -0,0 +1,17 @@
+<div id="menu">
+    <div class="text white text xtiny align center display block" id="name">Kamil Słotwiński</div>
+    <div id="user-icon" class="align center display block"><img src="img/photo.png" width="64" height="64" class="align center"/></div>
+    <nav class="navbar" role="navigation">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="menu_category">
+                Główne informacje
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="#" class="scroll_link" data-dest="#main" id="navbar-main">Menu1</a></li>
+                <li><a href="#" class="scroll_link" data-dest="#db_board" id="navbar-db">Menu2</a></li>
+                <li><a href="#" class="scroll_link" data-dest="#lang_board" id="navbar-lang">Menu3</a></li>
+                <li><a href="#" class="scroll_link" data-dest="#server_board" id="navbar-server">...</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
