c6ae554dfc2a82d6ed99e2e0f7b38c2087aadf17
solisqq <ebaza3@go2.pl>
2017-10-16 08:59:11 +0200

    Add files via upload

---
+++ portfolio/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'head.php'; ?>
+
+<body>
+
+    <?php include 'menu.php'; ?>
+    
+    <?php include 'footer.php'; ?>
+    
+</body>
+
+</html>
---
+++ portfolio/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function (){
+
+	var header_size=0;
+	var toggle_focus=0;
+
+	/*Pages*/
+	main_start_pos = 0;
+	/*/Pages*/
+
+	$('#cloud').addClass('active');
+	$('#name_main').addClass('active');
+
+	$(window).scroll(function() {//OnScrollChange
+		var position = $(document).scrollTop(); //Position
+		if(position>1) {
+			$('.triangle').addClass('no-anim');
+		} else {
+			$('.triangle').removeClass('no-anim');
+		}
+	});
+	$(".scroll_link").click(function (e){//Scroll Link
+		toggle_focus=1; 
+		e.preventDefault();
+		var destination = $(this).data('dest');
+		var scrollDestination = $(destination).offset().top-header_size;
+		this.focus();
+        $('html, body').animate({
+            scrollTop: scrollDestination
+        }, "slow", function(){ toggle_focus=0; });
+        console.log(destination);
+    });
+    $("#scroll_link").click(function (e){
+    	toggle_focus=1; 
+		e.preventDefault();
+		var destination = $(this).data('dest');
+		var scrollDestination = $(destination).offset().top-header_size;
+		this.focus();
+        $('html, body').animate({
+            scrollTop: scrollDestination
+        }, "slow", function(){ toggle_focus=0; });
+        console.log(destination);
+    });
+});
